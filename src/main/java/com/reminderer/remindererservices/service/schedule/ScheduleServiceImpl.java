a6e1917801d1c9a48d0b222ae90474fd5c7d704c
Ian Davidson <Ianmattdavidson@gmail.com>
2020-12-26 14:50:07 -0800

    2: added another service for a scheduler to use

---
+++ src/main/java/com/reminderer/remindererservices/service/schedule/ScheduleServiceImpl.java
@@ -0,0 +1,8 @@
+package com.reminderer.remindererservices.service.schedule;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ScheduleServiceImpl implements ScheduleService {
+
+}
